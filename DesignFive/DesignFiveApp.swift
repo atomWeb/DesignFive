cdca76f4bd67b1e7fd74b39f156699d606df0f95
nabusim <cristianjose@gmail.com>
2024-11-08 21:20:28 +0100

    Initial Commit

---
+++ DesignFive/DesignFiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignFiveApp.swift
+//  DesignFive
+//
+//  Created by Cristian Jose Perez Guerra on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DesignFiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
